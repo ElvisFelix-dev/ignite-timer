61a1b51d2b779a836c01b70a572227d7ccd68547
Elvis Felix <elvisjaspion@gmail.com>
2023-01-23 17:51:19 -0300

    configurando Global-Styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/deafult'
+import { GlobalStyle } from './styles/global'
+
 export function App() {
   return (
-    <h1>Timer</h1>
+    <>
+      <ThemeProvider theme={defaultTheme}>
+        <h1>Timer</h1>
+        <GlobalStyle />
+      </ThemeProvider>
+    </>
   )
 }
 
