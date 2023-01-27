92f17b792d209c0ec4e0c19fc7d4bfaed8cb1231
Elvis Felix <elvisjaspion@gmail.com>
2023-01-27 13:16:20 -0300

    Refatorando components

---
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(5, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser no minimo 5 minutos')
+    .max(60, 'O ciclo precisa ser no máximo 60 minutos'),
+})
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+export function NewCycleForm() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
+
+  return (
+    <FormContainer>
+      <label htmlFor="task">Vou trabalhar em</label>
+      <TaskInput
+        id="task"
+        list="task-suggestion"
+        placeholder="Dê um nome para seu projeto"
+        disabled={!!activeCycle}
+        {...register('task')}
+      />
+
+      <datalist id="task-suggestion">
+        <option value="Trabalhar com frontend" />
+        <option value="Refatorar components" />
+        <option value="Deploy" />
+        <option value="Estudar CSS3" />
+        <option value="Atualizar o responsivo" />
+        <option value="Desenvolver a versão mobile" />
+        <option value="Adicionar features" />
+      </datalist>
+
+      <label htmlFor="minutesAmount">durante</label>
+      <MinutesAmountInput
+        type="number"
+        id="minutesAmount"
+        placeholder="00"
+        step={5}
+        min={5}
+        max={60}
+        disabled={!!activeCycle}
+        {...register('minutesAmount', { valueAsNumber: true })}
+      />
+
+      <span>minutos.</span>
+    </FormContainer>
+  )
+}
