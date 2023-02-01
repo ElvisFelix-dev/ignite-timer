908c9cb92a9635919f430ff02d1f649648715daa
Elvis Felix <elvisjaspion@gmail.com>
2023-01-31 23:03:20 -0300

    add responsivo v1

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -5,10 +5,14 @@ export const LayoutContainer = styled.div`
   height: calc(100vh - 10rem);
   margin: 5rem;
   padding: 2.5rem;
-
   background: ${(props) => props.theme['gray-800']};
   border-radius: 8px;
-
   display: flex;
   flex-direction: column;
-`
+  
+  @media (max-width: 767px) {
+    max-width: 90%;
+    margin: 2.5rem auto;
+  }
+`;
+
