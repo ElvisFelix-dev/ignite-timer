e00fd846a62fde2169a0b2000401a93a3f7b7eb4
Elvis Felix <elvisjaspion@gmail.com>
2023-01-31 23:12:36 -0300

    Responsive v3

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -3,13 +3,18 @@ import styled from 'styled-components'
 export const HistoryContainer = styled.main`
   flex: 1;
   padding: 3.5rem;
-
   display: flex;
   flex-direction: column;
-
   h1 {
     font-size: 1.5rem;
     color: ${(props) => props.theme['gray-100']};
+  }
+
+  @media only screen and (max-width: 720px) {
+    padding: 1.5rem;
+    h1 {
+      font-size: 1.25rem;
+    }
   }
 `
 
@@ -17,12 +22,10 @@ export const HistoryList = styled.div`
   flex: 1;
   overflow: auto;
   margin-top: 2rem;
-
   table {
     width: 100%;
     border-collapse: collapse;
     min-width: 600px;
-
     th {
       background-color: ${(props) => props.theme['gray-600']};
       padding: 1rem;
@@ -30,32 +33,38 @@ export const HistoryList = styled.div`
       color: ${(props) => props.theme['gray-100']};
       font-size: 0.875rem;
       line-height: 1.6;
-
       &:first-child {
         border-top-left-radius: 8px;
         padding-left: 1.5rem;
       }
-
       &:last-child {
         border-top-right-radius: 8px;
         padding-right: 1.5rem;
       }
     }
-
     td {
       background-color: ${(props) => props.theme['gray-700']};
       border-top: 4px solid ${(props) => props.theme['gray-800']};
       padding: 1rem;
       font-size: 0.875rem;
       line-height: 1.6;
-
       &:first-child {
         width: 50%;
         padding-left: 1.5rem;
       }
-
       &:last-child {
         padding-right: 1.5rem;
+      }
+    }
+
+    @media only screen and (max-width: 720px) {
+      td {
+        font-size: 0.75rem;
+        padding: 0.75rem;
+      }
+      th {
+        font-size: 0.75rem;
+        padding: 0.75rem;
       }
     }
   }
@@ -75,7 +84,6 @@ export const Status = styled.span<StatusProps>`
   display: flex;
   align-items: center;
   gap: 0.5rem;
-
   &::before {
     content: '';
     width: 0.5rem;
