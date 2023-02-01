3090092ab5d06c3a9ace06f56ae82ff9239b51d5
Elvis Felix <elvisjaspion@gmail.com>
2023-01-31 23:08:41 -0300

    Responsivo v2

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,17 +2,21 @@ import styled from 'styled-components'
 
 export const HomeContainer = styled.main`
   flex: 1;
-
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-
   form {
     display: flex;
     flex-direction: column;
     align-items: center;
     gap: 3.5rem;
+  }
+  
+  @media (max-width: 767px) {
+    form {
+      gap: 1.5rem;
+    }
   }
 `
 
@@ -21,27 +25,26 @@ export const BaseCountdownButton = styled.button`
   border: 0;
   padding: 1rem;
   border-radius: 8px;
-
   display: flex;
   align-items: center;
   justify-content: center;
   color: ${(props) => props.theme['gray-100']};
-
   gap: 0.5rem;
   font-weight: bold;
-
   cursor: pointer;
-
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
+  }
+  
+  @media (max-width: 767px) {
+    padding: 0.5rem;
   }
 `
 
 export const StartCountdownButton = styled(BaseCountdownButton)`
   background: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme['gray-100']};
-
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
   }
@@ -49,7 +52,6 @@ export const StartCountdownButton = styled(BaseCountdownButton)`
 
 export const StopCountdownButton = styled(BaseCountdownButton)`
   background: ${(props) => props.theme['red-500']};
-
   &:not(:disabled):hover {
     background: ${(props) => props.theme['red-700']};
   }
